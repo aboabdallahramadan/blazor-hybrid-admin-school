0f3966036b5d0d76b8b6d36ac794d7fc05f2854b
mohammad ramadan <aboabdallahramadan@gmail.com>
2025-03-23 21:31:52 +0300

    initial commit agaim

--- obj/Debug/net8.0-ios/iossimulator-x64/MauiApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-x64/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5f84f6f33779045c69159faaa2ecbfd1b3c620ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
